304944518308bbcbe1a5c07e0e8f6a4f30f4d64f
mendoza256 <67234652+mendoza256@users.noreply.github.com>
2021-08-15 19:00:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('console works');
+
+//eintritt in rente
+const rente = new Date(2022, 06, 30).getTime();
+
+let x = setInterval(function() {
+    // heute
+    let now = new Date().getTime();
+
+    // differenz zw heute und rente
+    let difference = rente - now;
+
+    // rechnung für wochen und tage
+    let weeks = Math.floor(difference / (1000 * 60 * 60 * 24 * 7));
+    let days = Math.floor((difference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24))
+
+    //anzeige im div countdown
+    document.getElementById("countdown").innerHTML = `Hallo Papa, nur noch <br><br>${weeks} Wochen <br>${days} Tage <br><br>bis zu deiner Rente!`;
+
+    if (difference <= 0) {
+        document.getElementById("countdown").innerHTML = "Alles Liebe zum Eintritt in die wohlverdiente Rente! :)"
+    }
+}, 1000);
