c3b4c52f12c04b3f4304622a10e9946b3f0f1b5c
mendoza256 <67234652+mendoza256@users.noreply.github.com>
2021-08-15 19:11:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let x = setInterval(function() {
     let days = Math.floor((difference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24))
 
     //anzeige im div countdown
-    document.getElementById("countdown").innerHTML = `Hallo Papa, nur noch <br><br>${weeks} Wochen <br>${days} Tage <br><br>bis zu deiner Rente!`;
+    document.getElementById("countdown").innerHTML = `Hallo Papa, nur noch <br><br>${weeks} Wochen <br>${days} Tage <br><br>bis zu deiner Rente! &#x1F973`;
 
     if (difference <= 0) {
         document.getElementById("countdown").innerHTML = "Alles Liebe zum Eintritt in die wohlverdiente Rente! :)"
