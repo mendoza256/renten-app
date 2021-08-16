4e20411b72fb29bcf48ee1676146773253e1c67c
mendoza256 <67234652+mendoza256@users.noreply.github.com>
2021-08-16 09:41:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 console.log('console works');
 
-//eintritt in rente
+//datum - eintritt in rente
 const rente = new Date("Jul 02, 2022").getTime();
 
+// emojis
+const funEmojis = ["1F60D", "1F929", "1F917", "1F973", "1F920", "1F60E", "1F4AB", "1F918", "1F64C", "1F3CC", "1F99C", "1F490", "1F375", "1F37E", "1F942", "1F3DD", "1F3D6", "1F31F", "1F389", "1F388", "1F3C6", "1F947", "26F3"];
+
+// emoji-picker
+let randomEmoji = funEmojis[Math.floor(Math.random()*funEmojis.length)]
+
+// berechnung + aktualisierung
 const x = setInterval(function() {
     // heute
     let now = new Date().getTime();
@@ -15,9 +22,12 @@ const x = setInterval(function() {
     let days = Math.floor((difference % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24))
 
     //anzeige im div countdown
-    document.getElementById("countdown").innerHTML = `Hallo Papa, nur noch <br><br>${weeks} Wochen <br>${days} Tage <br><br>bis zu deiner Rente! &#x1F973`;
+    document.getElementById("countdown").innerHTML = `Hallo Papa, nur noch <br><br>${weeks} Wochen <br>${days} Tage <br><br>bis zu deiner Rente! &#x${randomEmoji}`;
 
     if (difference <= 0) {
         document.getElementById("countdown").innerHTML = "Alles Liebe zum Eintritt in die wohlverdiente Rente! :)"
     }
 }, 1000);
+
+
+
